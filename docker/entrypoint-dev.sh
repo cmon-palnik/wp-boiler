a65cca302133533df31fec3b63524c08ba597b05
Szymon Liszewski <szymon.liszewski@billennium.com>
2023-11-18 12:53:50 +0100

    Tidy up debug.log

--- docker/entrypoint-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp /run/secrets/id_rsa ~/.ssh/
-chmod 700 ~/.ssh/
-chmod 600 ~/.ssh/id_rsa
-ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; composer install --no-interaction --optimize-autoloader'
-
-php-fpm
