d6a112a5a0efb98f4b02a016c38773dc9daa0419
Szymon Liszewski <szymon.liszewski@billennium.com>
2024-08-17 12:49:58 +0200

    Add healthcheck

--- docker/apache2-wordpress.sh
+++ docker/apache2-wordpress.sh
@@ -2,8 +2,8 @@
 set -eux
 cd /var/www/html/
 
-wp core is-installed --allow-root || wp core install --allow-root --url=localhost:8080 --title=Logs2ELK \
-        --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
+wp core is-installed --allow-root || wp core install --allow-root --url=localhost --title=WPBoiler \
+        --admin_user=admin --admin_password=strongpassword --admin_email=info@example.com
 
 chown -R www-data:www-data /var/www/html
 chmod -R g+w /var/www/html
